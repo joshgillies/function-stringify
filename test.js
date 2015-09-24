e68414add66c577193983c164788c3c0addd5bf2
Josh Gillies <github@joshgilli.es>
2015-09-25 09:52:40 +1000

    More tests/examples

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var fnStringify = require('./')
 
 test('simple test', function (assert) {
-  var template = fnStringify(function () {/*
+  var html = fnStringify(function () {/*
     <!DOCTYPE html>
     <html>
       <head>
@@ -26,5 +26,37 @@ test('simple test', function (assert) {
   ].join('\n')
 
   assert.plan(1)
-  assert.equals(template, expected)
+  assert.equals(html, expected)
+})
+
+test('stringify call inline', function (assert) {
+  function html () {
+    /*
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Welcome to The Internet</title>
+        </head>
+        <body>
+          <h1>Hello, world!</h1>
+        </body>
+      </html>
+    */
+
+    return fnStringify(html)
+  }
+  var expected = [
+    '<!DOCTYPE html>',
+    '<html>',
+    '  <head>',
+    '    <title>Welcome to The Internet</title>',
+    '  </head>',
+    '  <body>',
+    '    <h1>Hello, world!</h1>',
+    '  </body>',
+    '</html>'
+  ].join('\n')
+
+  assert.plan(1)
+  assert.equals(html(), expected)
 })
